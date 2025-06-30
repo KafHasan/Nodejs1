fea2fc76a45ab046069eeb460909dc32a6c91133
Hasan Al-Kaf <kaf@dcc.kfupm.edu.sa>
2025-07-01 01:57:03 +0300

    Update project with latest changes

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
+const port=5000 || process.env.port;
 const app = express();
 app.use(cors());
 app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/authDemo");
 app.use("/api/auth", authRoutes);
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("✅ Backend running on http://localhost:5000");
 });
