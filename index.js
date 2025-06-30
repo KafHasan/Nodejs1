005c719e75468e0ba061968f52441eb04dffb67a
Hasan Al-Kaf <kaf@dcc.kfupm.edu.sa>
2025-07-01 02:06:18 +0300

    Update project with latest changes

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port=5000 || process.env.port;
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect("mongodb://localhost:27017/authDemo");
+//mongoose.connect("mongodb://localhost:27017/authDemo");
+mongoose.connect("mongodb+srv://kfupmdcc2010:Hasan123@cluster0.dviam6i.mongodb.net/authDemo");
+
 app.use("/api/auth", authRoutes);
 app.listen(port, () => {
   console.log("✅ Backend running on http://localhost:5000");
